382728a9400de303792578c488b84f9647edcd00
Tony-Tsoi <wctsoi@ucdavis.edu>
2020-04-15 15:36:16 -0700

    assume fifo in square

--- hdl/squareUnit.v
+++ hdl/squareUnit.v
@@ -1,8 +1,8 @@
-module cellUnit (irrdi, irrui, irddi, irdi, iri, irui, iruui, idi, iui, 
+module squareUnit (irrdi, irrui, irddi, irdi, iri, irui, iruui, idi, iui, 
 ilddi, ildi, ili, ilui, iluui, illdi, illui,
 orrdo, orruo, orddo, ordo, oro, oruo, oruuo, odo, ouo, 
 olddo, oldo, olo, oluo, oluuo, olldo, olluo,
-clk, xpos, ypos, cpiece, newboard, done);
+clk, xpos, ypos, cpiece, newboard, done, fifoOut);
 // TODO: add output to column unit FIFO
 // TODO: for every time send to move list, assert done_c = 1'b0;
 
@@ -12,6 +12,7 @@ input [2:0] xpos, ypos; // specifies the position of the unit on the board
 input [3:0] cpiece; // current piece occupied at this spot
 
 output reg done; // done signal
+output [47:0] fifoOut; // output of FIFO
 
 // inputs and outputs to neighbor cells
 input [8:0] irrdi, irrui, irddi, irdi, iri, irui, iruui, idi, iui, 
@@ -32,6 +33,17 @@ parameter KING = 3'o6;
 parameter NOTUSED = 3'o7;
 parameter PVOID = 9'h0; // it's just {3'o0, 3'o0, EMPTY} - denotes an empty space at xpos = 0, ypos = 0
 parameter ROW3 = 3'o3; // value for ypos to be at row 3 (for pawn advance two blocks forward
+
+// Moves Output
+reg [5:0] mvrrd, mvrru, mvrdd, mvruu, mvldd, mvluu, mvlld, mvllu;
+reg [5:0] mvrd, mvr, mvru, mvd, mvu, mvld, mvl, mvlu;
+reg [47:0] wr1 = {mvrd, mvr, mvru, mvd, mvu, mvld, mvl, mvlu};
+reg [47:0] wr2 = {mvrrd, mvrru, mvrdd, mvruu, mvldd, mvluu, mvlld, mvllu};
+wire wren1 = |{wr1};
+wire wren2 = |{wr2};
+
+// FIFO Module Declaration
+MyFifo F1F0 (.clk(clk), .wr1(wr1), .wr2(wr2), .rd1(fifoOut), .wren1(wren1), .wren2(wren2));
 
 // done signal
 reg done_c;
@@ -127,7 +139,7 @@ always @(*) begin
 		if (cpiece[2:0] == EMPTY) begin
 			// if current place is empty, pawn can move up
 			if (iui[2:0] != EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvu = iui[8:3];
 				
 				if (iui[5:3] == ROW2) begin
 					// if started from ROW2
@@ -139,10 +151,10 @@ always @(*) begin
 			if (cpiece[3] == BLACK) begin
 				// if current place isn't empty and is of opposite, pawn can take sideway
 				if (irui[2:0] == PAWN) begin
-					// PSEUDOCODE: send to move list (StML)
+					mvru = irui[8:3];
 				end
 				if (ilui]2:0] == PAWN) begin
-					// PSEUDOCODE: send to move list (StML)
+					mvlu = ilui[8:3];
 				end
 			end
 		end
@@ -150,135 +162,127 @@ always @(*) begin
 		// KNIGHT case
 		if ((cpiece[2:0] == EMPTY) || (cpiece[3] == BLACK)) begin
 			if (irrdi[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvrrd = irrdi[8:3];
 			if (irrui[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvrru = irrui[8:3];
 			if (irddi[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvrdd = irddi[8:3];
 			if (iruui[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvruu = iruui[8:3];
 			if (ilddi[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvldd = ilddi[8:3];
 			if (iluui[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvluu = iluui[8:3];
 			if (illdi[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvlld = illdi[8:3];
 			if (illui[2:0] != EMPTY)
-				// PSEUDOCODE: send to move list (StML)
-			end
+				mvllu = illui[8:3];
 		end
 		
 		// RU and LU is available for pawn, if can take current piece
 		if (irui[2:0] != EMPTY) begin
 			if ((cpiece[2:0] == EMPTY) && (irui[2:0] != PAWN)) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvru = irui[8:3];
 				
 				if ((irui[2:0] == BISHOP) && (irui[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					oruo_c = irui;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvru = irui[8:3];
 			end
 		end
 		
 		if (ilui[2:0] != EMPTY) begin
 			if ((cpiece[2:0] == EMPTY) && (ilui[2:0] != PAWN)) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvlu = ilui[8:3];
 				
 				if ((ilui[2:0] == BISHOP) && (ilui[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					oruo_c = ilui;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvlu = ilui[8:3];
 			end
 		end
 		
 		// U is available for pawn if empty
 		if (iui[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvu = iui[8:3];
 				
 				if ((iui[2:0] == BISHOP) && (iui[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					oruo_c = irui;
 			end
 			if ((cpiece[3] == BLACK) && (iui[2:0] != PAWN)) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvu = iui[8:3];
 			end
 		end
 		
 		// 5 remaining neighboring positions
 		if (irdi[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvrd = irdi[8:3];
 				
 				if ((irdi[2:0] == BISHOP) && (irdi[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					ordo_c = irdi;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvrd = irdi[8:3];
 			end
 		end
 		
 		if (ildi[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvld = ildi[8:3];
 				
 				if ((ildi[2:0] == BISHOP) && (ildi[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					oldo_c = ildi;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvld = ildi[8:3];
 			end
 		end
 		
 		if (idi[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvd = idi[8:3];
 				
 				if ((idi[2:0] == BISHOP) && (idi[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					odo_c = idi;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvd = idi[8:3];
 			end
 		end
 		
 		if (iri[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
-				
+				mvr = iri[8:3];
+
 				if ((iri[2:0] == BISHOP) && (iri[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					oro_c = iri;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvr = iri[8:3];
 			end
 		end
 		
 		if (ili[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvl = ili[8:3];
 				
 				if ((ili[2:0] == BISHOP) && (ili[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
 					olo_c = ili;
 			end
 			if (cpiece[3] == BLACK) begin
-				// PSEUDOCODE: send to move list (StML)
+				mvl = ili[8:3];
 			end
 		end
 	end
