99556398823f9f47a759304a98a353295f10114c
Tony-Tsoi <wctsoi@ucdavis.edu>
2020-04-19 14:40:58 -0700

    add done_c, anticipate change states

--- hdl/squareUnit.v
+++ hdl/squareUnit.v
@@ -3,8 +3,6 @@ ilddi, ildi, ili, ilui, iluui, illdi, illui,
 orrdo, orruo, orddo, ordo, oro, oruo, oruuo, odo, ouo, 
 olddo, oldo, olo, oluo, oluuo, olldo, olluo,
 clk, xpos, ypos, cpiece, newboard, done, fifoOut);
-// TODO: add output to column unit FIFO
-// TODO: for every time send to move list, assert done_c = 1'b0;
 
 input clk; // clock input
 input newboard; // if is new board, put self at the outgoing positions
@@ -33,6 +31,8 @@ parameter KING = 3'o6;
 parameter NOTUSED = 3'o7;
 parameter PVOID = 9'h0; // it's just {3'o0, 3'o0, EMPTY} - denotes an empty space at xpos = 0, ypos = 0
 parameter ROW3 = 3'o3; // value for ypos to be at row 3 (for pawn advance two blocks forward
+parameter ENDMOV = {6'o00, xpos, ypos, 6'o00, xpos, ypos, 6'o00, xpos, ypos, 6'o00, xpos, ypos,
+	6'o00, xpos, ypos, 6'o00, xpos, ypos, 6'o00, xpos, ypos, 6'o00, xpos, ypos}; // end squence for move list
 
 // Moves Output
 reg [5:0] mvrrd, mvrru, mvrdd, mvruu, mvldd, mvluu, mvlld, mvllu;
@@ -140,6 +140,7 @@ always @(*) begin
 			// if current place is empty, pawn can move up
 			if (iui[2:0] != EMPTY) begin
 				mvu = iui[8:3];
+				done_c = 1'b0;
 				
 				if (iui[5:3] == ROW2) begin
 					// if started from ROW2
@@ -152,37 +153,56 @@ always @(*) begin
 				// if current place isn't empty and is of opposite, pawn can take sideway
 				if (irui[2:0] == PAWN) begin
 					mvru = irui[8:3];
+					done_c = 1'b0;
 				end
 				if (ilui]2:0] == PAWN) begin
 					mvlu = ilui[8:3];
+					done_c = 1'b0;
 				end
 			end
 		end
 		
 		// KNIGHT case
 		if ((cpiece[2:0] == EMPTY) || (cpiece[3] == BLACK)) begin
-			if (irrdi[2:0] != EMPTY)
+			if (irrdi[2:0] != EMPTY) begin
 				mvrrd = irrdi[8:3];
-			if (irrui[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (irrui[2:0] != EMPTY) begin
 				mvrru = irrui[8:3];
-			if (irddi[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (irddi[2:0] != EMPTY) begin
 				mvrdd = irddi[8:3];
-			if (iruui[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (iruui[2:0] != EMPTY) begin
 				mvruu = iruui[8:3];
-			if (ilddi[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (ilddi[2:0] != EMPTY) begin
 				mvldd = ilddi[8:3];
-			if (iluui[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (iluui[2:0] != EMPTY) begin
 				mvluu = iluui[8:3];
-			if (illdi[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (illdi[2:0] != EMPTY) begin
 				mvlld = illdi[8:3];
-			if (illui[2:0] != EMPTY)
+				done_c = 1'b0;
+			end
+			if (illui[2:0] != EMPTY) begin
 				mvllu = illui[8:3];
+				done_c = 1'b0;
+			end
 		end
 		
 		// RU and LU is available for pawn, if can take current piece
 		if (irui[2:0] != EMPTY) begin
 			if ((cpiece[2:0] == EMPTY) && (irui[2:0] != PAWN)) begin
 				mvru = irui[8:3];
+				done_c = 1'b0;
 				
 				if ((irui[2:0] == BISHOP) && (irui[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -190,12 +210,14 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvru = irui[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
 		if (ilui[2:0] != EMPTY) begin
 			if ((cpiece[2:0] == EMPTY) && (ilui[2:0] != PAWN)) begin
 				mvlu = ilui[8:3];
+				done_c = 1'b0;
 				
 				if ((ilui[2:0] == BISHOP) && (ilui[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -203,6 +225,7 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvlu = ilui[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
@@ -210,6 +233,7 @@ always @(*) begin
 		if (iui[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
 				mvu = iui[8:3];
+				done_c = 1'b0;
 				
 				if ((iui[2:0] == BISHOP) && (iui[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -217,6 +241,7 @@ always @(*) begin
 			end
 			if ((cpiece[3] == BLACK) && (iui[2:0] != PAWN)) begin
 				mvu = iui[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
@@ -224,6 +249,7 @@ always @(*) begin
 		if (irdi[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
 				mvrd = irdi[8:3];
+				done_c = 1'b0;
 				
 				if ((irdi[2:0] == BISHOP) && (irdi[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -231,12 +257,14 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvrd = irdi[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
 		if (ildi[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
 				mvld = ildi[8:3];
+				done_c = 1'b0;
 				
 				if ((ildi[2:0] == BISHOP) && (ildi[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -244,12 +272,14 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvld = ildi[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
 		if (idi[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
 				mvd = idi[8:3];
+				done_c = 1'b0;
 				
 				if ((idi[2:0] == BISHOP) && (idi[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -257,12 +287,14 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvd = idi[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
 		if (iri[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
 				mvr = iri[8:3];
+				done_c = 1'b0;
 
 				if ((iri[2:0] == BISHOP) && (iri[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -270,12 +302,14 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvr = iri[8:3];
+				done_c = 1'b0;
 			end
 		end
 		
 		if (ili[2:0] != EMPTY) begin
 			if (cpiece[2:0] == EMPTY) begin
 				mvl = ili[8:3];
+				done_c = 1'b0;
 				
 				if ((ili[2:0] == BISHOP) && (ili[2:0] == QUEEN))
 					// if bishop or queen, propogate diag
@@ -283,6 +317,7 @@ always @(*) begin
 			end
 			if (cpiece[3] == BLACK) begin
 				mvl = ili[8:3];
+				done_c = 1'b0;
 			end
 		end
 	end
