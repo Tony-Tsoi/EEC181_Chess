c6ed0eb7a8120e18cc4aa2d38d366db4ec0ee369
kenneth51 <59973955+kenneth51@users.noreply.github.com>
2020-05-22 09:45:34 -0700

    More testing stuff
    control_TestOnly.v is a modified version of control.v so we can add more outputs to view internal signals.

--- test/control_tb.v
+++ test/control_tb.v
@@ -19,8 +19,9 @@ reg [DATA_WIDTH-1:0] slave_writedata;
 reg [(DATA_WIDTH/8)-1:0] slave_byteenable; //Not currently used
 
 reg readdata;
+wire lmgdone;
 
-control control_inst1(
+control_TestOnly control_inst1(
 	// signals to connect to an Avalon clock source interface
 	.clk (clk),
 	.reset (reset),
@@ -31,7 +32,8 @@ control control_inst1(
 	.slave_write (slave_write),
 	.slave_readdata (slave_readdata),
 	.slave_writedata (slave_writedata),
-	.slave_byteenable (slave_byteenable)
+	.slave_byteenable (slave_byteenable),
+	.lmgdone(lmgdone)
 );
 
 initial begin
@@ -51,10 +53,12 @@ initial begin
 	// To make things easy, assign all 4 values whenever you want to read/write something.
 	// Normally, slave_read should only be high for 3 cycles, and slave_write should be high for 1 cycle,
 	// For simulation purposes, just do 4 cycles minimum between read/writes.
-	slave_address = 15'd0;
-	slave_read = 1'b0;
+	
+	//====================== WRITING BOARD STATE =====================
+	slave_address = 15'd2;
+	slave_read = 1'b1;
 	slave_write = 1'b1;
-	slave_writedata = 32'h0000_0001; // Edit this
+	slave_writedata = 32'h2346_5432; 
 	
 	repeat(4) begin
 		clk = 1'b1;
@@ -62,19 +66,118 @@ initial begin
 		clk = 1'b0;
 		#100;
 	end
-
-	slave_address = 15'd0; // These 4 are just place holder values, edit these to read/write something useful.
-	slave_read = 1'b1;
-	slave_write = 1'b0;
-	slave_writedata = 32'h0000_0000; // Edit this
-
 	
-	repeat(1000) begin
+	slave_address = 15'd3;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
 		clk = 1'b1;
 		#100;
 		clk = 1'b0;
 		#100;
 	end
+	
+	slave_address = 15'd4;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	slave_address = 15'd5;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	slave_address = 15'd6;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	slave_address = 15'd7;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	slave_address = 15'd8;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	slave_address = 15'd9;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0000; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	//====================== END WRITING BOARD STATE =====================
+	
+	slave_address = 15'd0;
+	slave_read = 1'b1;
+	slave_write = 1'b1;
+	slave_writedata = 32'h0000_0001; 
+	
+	repeat(4) begin
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	repeat(10000) begin
+		slave_address = 15'd0; // These 4 are just place holder values, edit these to read/write something useful.
+		slave_read = 1'b1;
+		slave_write = 1'b0;
+		slave_writedata = 32'h0000_0000; // Edit this
+
+	
+		clk = 1'b1;
+		#100;
+		clk = 1'b0;
+		#100;
+	end
+	
+	
 	
 
 
